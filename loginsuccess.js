9c7c4e9defbf26953e0001b71c0693c4d3df1fa4
davepokpong <davepokpong@gmail.com>
2021-02-20 16:33:45 +0700

    update

---
+++ loginsuccess.js
@@ -0,0 +1,26 @@
+const username_ele = document.getElementById("username");
+const password_ele = document.getElementById("password");
+
+function login(){
+    var username = username_ele.value;
+    var password = password_ele.value;
+    console.log(username)
+    console.log(password)
+    if (username == "admin1" && password == "1234"){
+        alert("Login successfully1!")
+        location.href = "./admin.html"
+    }
+    else if (username == "admin2" && password == "12345"){
+        alert("Login successfully!")
+        location.href = "admin2.html"
+    }
+    else if (username == "admin3" && password == "123456"){
+        alert("Login successfully!")
+        location.href = "admin3.html"
+    }
+    else{
+        document.getElementById("errormsg").innerHTML = "Invalid username or password";
+    }
+    username_ele.value = "";
+    password_ele.value = "";
+}
